04040f5248b1ec3531bd338f863973638ca876b2
florian <florian@barras.io>
2018-11-17 18:24:12 +0100

    Formatting

--- java/jupiter/integration/android/source/jupiter/integration/android/io/AIO.java
+++ java/jupiter/integration/android/source/jupiter/integration/android/io/AIO.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright © 2013-2018 Florian Barras <https://barras.AIO>
+ * Copyright © 2013-2018 Florian Barras <https://barras.io> (florian@barras.io)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
