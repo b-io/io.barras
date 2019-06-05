7f6e455be9073f15dd9592e34451212b4b36adcc
florian <florian@barras.io>
2019-06-06 01:33:49 +0200

    Resources

--- java/jupiter/common/source/jupiter/common/io/Resources.java
+++ java/jupiter/common/source/jupiter/common/io/Resources.java
@@ -114,4 +114,22 @@ public class Resources {
 		}
 		return false;
 	}
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// VERIFIERS
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Tests whether the specified {@link Class} is assignable to a {@link Closeable} or an
+	 * {@link AutoCloseable}.
+	 * <p>
+	 * @param c the {@link Class} to test
+	 * <p>
+	 * @return {@code true} if the specified {@link Class} is assignable to a {@link Closeable} or
+	 *         an {@link AutoCloseable}, {@code false} otherwise
+	 */
+	public static boolean is(final Class<?> c) {
+		return Closeable.class.isAssignableFrom(c) || AutoCloseable.class.isAssignableFrom(c);
+	}
 }
