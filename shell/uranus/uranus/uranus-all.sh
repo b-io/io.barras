8141ca53c0d57e4d62bf07d268ac98b63fd71e7a
florian <florian@barras.io>
2020-01-21 23:50:24 +0100

    Clean

--- shell/uranus/uranus/uranus-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-. "/usr/lib/uranus/ura.sh"
-. "/usr/lib/uranus/ura-yum.sh"
-. "/usr/lib/uranus/ura-pkg.sh"
-. "/usr/lib/uranus/ura-rpm.sh"
-. "/usr/lib/uranus/ura-pms.sh"
-. "/usr/lib/uranus/ura-gpg.sh"
-. "/usr/lib/uranus/ura-lxc.sh"
