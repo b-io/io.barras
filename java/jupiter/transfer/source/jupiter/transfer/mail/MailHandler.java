b7ee23120ddbba5767bee21c7ac6b7824f3c3b3b
florian <florian@barras.io>
2020-02-02 02:30:56 +0100

    Setup

--- java/jupiter/transfer/source/jupiter/transfer/mail/MailHandler.java
+++ java/jupiter/transfer/source/jupiter/transfer/mail/MailHandler.java
@@ -1,10 +1,10 @@
 /*
- * The MIT License
+ * The MIT License (MIT)
  *
- * Copyright © 2013-2019 Florian Barras <https://barras.io>
+ * Copyright © 2013-2020 Florian Barras <https://barras.io> (florian@barras.io)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation mails (the "Software"), to deal
+ * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
