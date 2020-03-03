dc010da7dc6aa47fc866378dd3a46b6826ba0944
florian <florian@barras.io>
2020-03-03 21:45:08 +0100

    Comparables

---
+++ java/jupiter/common/source/jupiter/common/struct/list/SortedUniqueList.java
@@ -0,0 +1,119 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright © 2013-2020 Florian Barras <https://barras.io> (florian@barras.io)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package jupiter.common.struct.list;
+
+import java.util.Collection;
+
+/**
+ * {@link SortedList} extends {@link ExtendedLinkedList} of {@code E} element type.
+ * <p>
+ * @param <E> the self element {@link Comparable} type of the {@link SortedList}
+ */
+public class SortedList<E extends Comparable<E>>
+		extends ExtendedLinkedList<E> {
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// CONSTANTS
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * The generated serial version ID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// CONSTRUCTORS
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Constructs an empty {@link SortedList} of {@code E} element type.
+	 */
+	public SortedList() {
+		super();
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Constructs a {@link SortedList} of {@code E} element type with the specified elements.
+	 * <p>
+	 * @param elements an {@code E} array
+	 * <p>
+	 * @throws NullPointerException if {@code elements} is {@code null}
+	 */
+	@SuppressWarnings("unchecked")
+	public SortedList(final E... elements) {
+		super(elements);
+	}
+
+	/**
+	 * Constructs a {@link SortedList} of {@code E} element type with the specified elements in a
+	 * {@link Collection}.
+	 * <p>
+	 * @param elements a {@link Collection} of {@code E} element subtype
+	 * <p>
+	 * @throws NullPointerException if {@code elements} is {@code null}
+	 */
+	public SortedList(final Collection<? extends E> elements) {
+		super(elements);
+	}
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// FUNCTIONS
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+	@Override
+	public synchronized boolean add(final E element) {
+		int index = 0;
+		for (final E e : this) {
+			if (e.compareTo(element) > 0) {
+				add(index, element);
+				return true;
+			}
+			++index;
+		}
+		super.add(element);
+		return true;
+	}
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	// OBJECT
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Creates a copy of {@code this}.
+	 * <p>
+	 * @return a copy of {@code this}
+	 *
+	 * @see jupiter.common.model.ICloneable
+	 */
+	@Override
+	@SuppressWarnings("unchecked")
+	public SortedList<E> clone() {
+		return (SortedList<E>) super.clone();
+	}
+}
