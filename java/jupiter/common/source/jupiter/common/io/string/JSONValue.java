0af6994dbbce03d80f494503f00df3f003d3dd93
Florian <florian@gzcim.com>
2020-05-01 16:22:57 +0200

    JSON

--- java/jupiter/common/source/jupiter/common/io/string/JSONValue.java
+++ java/jupiter/common/source/jupiter/common/io/string/JSONValue.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package jupiter.transfer.file;
+package jupiter.common.io.string;
 
 import java.io.Serializable;
 
