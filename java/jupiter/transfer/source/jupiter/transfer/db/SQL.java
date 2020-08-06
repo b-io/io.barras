12136dd9ea43ef0fb3ef635661d8324c6f21e633
florian <florian@barras.io>
2020-08-06 14:12:00 +0200

    SQL

--- java/jupiter/transfer/source/jupiter/transfer/db/SQL.java
+++ java/jupiter/transfer/source/jupiter/transfer/db/SQL.java
@@ -435,10 +435,10 @@ public class SQL {
 				Arrays.isNonEmpty(columns) ? Strings.joinWith(columns, ",", BRACKETER) : "*",
 				" FROM ", Strings.bracketize(table),
 				Arrays.isNonEmpty(conditionalColumns) ?
-				Strings.join(" WHERE ",
-						Strings.joinWith(conditionalColumns, "=? AND ", BRACKETER)
-						.concat("=?")) :
-				EMPTY);
+						Strings.join(" WHERE ",
+								Strings.joinWith(conditionalColumns, "=? AND ", BRACKETER)
+										.concat("=?")) :
+						EMPTY);
 	}
 
 	public static PreparedStatement createSelectStatement(final Connection connection,
@@ -506,10 +506,10 @@ public class SQL {
 		return Strings.join("UPDATE ", Strings.bracketize(table),
 				" SET ", Strings.joinWith(columns, "=?,", BRACKETER).concat("=?"),
 				Arrays.isNonEmpty(conditionalColumns) ?
-				Strings.join(" WHERE ",
-						Strings.joinWith(conditionalColumns, "=? AND ", BRACKETER)
-						.concat("=?")) :
-				EMPTY);
+						Strings.join(" WHERE ",
+								Strings.joinWith(conditionalColumns, "=? AND ", BRACKETER)
+										.concat("=?")) :
+						EMPTY);
 	}
 
 	public static PreparedStatement createUpdateStatement(final Connection connection,
@@ -544,10 +544,10 @@ public class SQL {
 		// Create the SQL query for deleting the table with the conditional columns
 		return Strings.join("DELETE FROM ", Strings.bracketize(table),
 				Arrays.isNonEmpty(conditionalColumns) ?
-				Strings.join(" WHERE ",
-						Strings.joinWith(conditionalColumns, "=? AND ", BRACKETER)
-						.concat("=?")) :
-				EMPTY);
+						Strings.join(" WHERE ",
+								Strings.joinWith(conditionalColumns, "=? AND ", BRACKETER)
+										.concat("=?")) :
+						EMPTY);
 	}
 
 	public static PreparedStatement createDeleteStatement(final Connection connection,
